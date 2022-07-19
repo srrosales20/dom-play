d8497218d084dfa932e777d962467d874d4a9716
srrosales20 <rosalessonia217@gmail.com>
2022-07-18 21:50:59 -0700

    ok

---
+++ js/dom-play.js
@@ -0,0 +1,14 @@
+//alert("Hi There");
+
+let actors = document.querySelectorAll(".hamlet");
+
+console.log(actors);
+
+//const myNodelist = document.querySelectorAll("p");
+// for (let i = 0; i < actors.length; i++) {
+//     actors[i].style.backgroundColor = "yellow";
+// }
+
+//actors.forEach(highlight);
+
+//document.querySelector(".hamlet").style.backgroundColor = "yellow";
